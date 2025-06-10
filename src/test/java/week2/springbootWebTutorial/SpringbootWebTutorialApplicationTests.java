bfd5c08ab230b2e032bc817ce3fae0828830344a
Ujjawal dev-010 <dev-010@icingtree.com>
2025-06-10 15:34:58 +0530

    Initial folder structure

---
+++ src/test/java/week2/springbootWebTutorial/SpringbootWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package week2.springbootWebTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
